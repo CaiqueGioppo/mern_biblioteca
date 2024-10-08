bc80e58a316d08670fe40dbade429e93b265aa9f
The_Potato <134632740+CaiqueGioppo@users.noreply.github.com>
2024-10-08 14:55:18 +0000

    commit3

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,58 @@
 //Rsskt440ofPY4GGU//
 import "dotenv/config";
 import express from "express";
-import mongoose from "mongoose";
 
-export async function connectToDatabase() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to the database");
-  } catch (error) {
-    console.error("Error connecting to the database: ", error);
-    process.exit(1);
-  }
-}
+import { connectToDatabase } from "./config/db.js";
+import { Book } from "./model/book.model.js";
 
 const app = express();
 
-app.listen(4500, () => {
-  console.log("Server is running on port 3000");
+app.use(express.json());
+
+app.post("/api/v1/books", async (req, res) => {
+  const { title, subtitle, author, genre, cover } = req.body;
+
+  try {
+    const book = new Book({ title, subtitle, author, genre, cover });
+    await book.save();
+    res.status(201).json({ success: true, data: book });
+  } catch (error) {
+    console.error("Error saving book: ", error);
+    res.status(500).json({ success: false, error: "Error saving book" });
+  }
+});
+
+app.get("/api/v1/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json({ success: true, data: books });
+  } catch (error) {
+    console.error("Error fetching books: ", error);
+    res.status(500).json({ sucess: false, error: "Error fetching books" });
+  }
+});
+
+app.get("/api/v1/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ sucess: false, error: "Invalid ID" });
+  }
+
+  try {
+    const book = await Book.findById(id);
+
+    if (!book) {
+      return res.status(404).json({ success: false, error: "Book not found" });
+    }
+
+    res.status(200).json({ sucess: true, data: book });
+  } catch (error) {
+    console.error("Error fetching book: ", error);
+    res.status(500).json({ success: false, error: "Error fetching book" });
+  }
+});
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
   connectToDatabase();
 });
